03431cc57c89777846f7640dbf2c5ac3d35ab6be
Quandrix Blackburn <quandrixblackburn@Quandrixs-MacBook-Air.local>
2026-02-15 16:33:17 -0600

    Bump PWA cache version and purge old caches

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,33 @@
-const CORE=['./','./index.html','./manifest.webmanifest','./integrations.js','./icon-180.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open('aia-v9').then(c=>c.addAll(CORE)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE_NAME = "aia-v10";
+const CORE = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./integrations.js",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
